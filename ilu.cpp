daff8cccbabf381e0d369c63a95f5a88f6fabd5f
Franciszek Sobota <sobotafran@gmail.com>
2026-06-01 17:56:39 +0200

    okeanos sync

--- ilu.cpp
+++ ilu.cpp
@@ -1067,7 +1067,7 @@ void ILU_solve(struct ILUFact *ilu, double *b, double *res) {
     std::vector<double> b_vec(b, b + ilu->num_rows_local);
     b_vec = utils::permutation::apply_permutation(b_vec, ilu->inv_perm);
     b_vec = dist_async_solve(ilu, b_vec, SolveType::L);
-    b_vec = dist_async_solve(ilu, b_vec, SolveType::U);
+    //b_vec = dist_async_solve(ilu, b_vec, SolveType::U);
 
     memcpy(res, b_vec.data(), ilu->num_rows_local * sizeof(double));
 }
